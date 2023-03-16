b58a0dd982743d27659f9b6f00622e9ffd576e01
Robert Bridda <100780112+robertbridda@users.noreply.github.com>
2023-03-16 16:50:10 +0100

    Aggiunta di un messaggio più informativo su / (root)

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Benvenuto. Per consultare la documentazione dell'api puoi andare su /api";
   }
 }
