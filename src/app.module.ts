f4f71dc89d100958eb9bb5f5fc02f9638c3760e9
Robert Bridda <briddarobert@icloud.com>
2023-05-14 17:09:25 +0200

    Aggiunto collegamento al database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,21 @@ import { EdgeModule } from './edges/edge.module';
 import { VertexModule } from './vertices/vertex.module';
 import { RouteModule } from './routes/route.module';
 import { MapModule } from './maps/map.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MapModule, EdgeModule, VertexModule, RouteModule],
+  imports: [MapModule, EdgeModule, VertexModule, RouteModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    database: 'bussola',
+    entities: [],
+    synchronize: true,
+  }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule { }
